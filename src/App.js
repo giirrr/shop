7df8207ac9d2dce2114d254a80c70ec8a8e7e4e9
giirrr <ikl1121@naver.com>
2023-08-31 16:55:22 +0900

    신발 사이트 숙제 시작

--- src/App.js
+++ src/App.js
@@ -4,22 +4,59 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { useState } from "react";
+import data from "./data.js";
 // import { Button, Nav, Navbar, Container } from "react-bootstrap";
 
 function App() {
+  let [shoes, setShoes] = useState(data);
   return (
     <div className="App">
-      <Navbar bg="primary" data-bs-theme="dark">
+      <Navbar bg="primary" data-bs-theme="dark" className="shop_navbar">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home" className="shop_navbarNavbar">
+            Shopping Pro
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <Button variant="primary">Primary</Button>{" "}
+      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
+            {/*             
+          그냥 입력하면 텍스트로 들어가고 {}로 감싸 줘야함
+            []대괄호는 보통 array [2]이런식
+            {}중괄호는 보통 object .title .id이런식  */}
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price} 원</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+            />
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price} 원</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            />
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price} 원</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
