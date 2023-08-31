f7ad5492b1e659eb27d09b27de932dff93271b66
giirrr <ikl1121@naver.com>
2023-09-01 02:08:42 +0900

    styled-components 사용해봄

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,12 +1,38 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+// let YellowBtn = styled.button`
+//   background: green;
+//   color: white;
+//   padding: 10px;
+//   border: none;
+// `;
+let YellowBtn = styled.button`
+  background: ${(props) => props.namom};
+  color: ${(props) => (props.namom == "blue" ? "white" : "yellow")};
+  // color: ${(e) => (e.namom == "blue" ? "white" : "yellow")};
+  padding: 10px;
+  border: none;
+`;
+
+let NewBtn = styled.button(YellowBtn);
+
+let Box = styled.div`
+  background: grey;
+  padding: 12px;
+`;
 
 function Detail(props) {
   let { iid } = useParams();
-  const detail = props.shoes.find((e) => e.id == iid);
+  const 상품 = props.shoes.find((e) => e.id == iid);
   console.log(iid);
-  console.log(detail);
+  console.log(상품);
   return (
     <div className="container">
+      <Box>
+        <YellowBtn namom="blue">버튼</YellowBtn>
+        <YellowBtn namom="green">버튼</YellowBtn>
+      </Box>
       <div className="row">
         <div className="col-md-6">
           <img
@@ -15,10 +41,10 @@ function Detail(props) {
           />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{props.shoes[detail.id].title}</h4>
-          <p>{props.shoes[detail.id].content}</p>
-          <p>{detail.content}</p>
-          <p>{detail.price}</p>
+          <h4 className="pt-5">{props.shoes[상품.id].title}</h4>
+          <p>{props.shoes[상품.id].content}</p>
+          <p>{상품.content}</p>
+          <p>{상품.price}</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
