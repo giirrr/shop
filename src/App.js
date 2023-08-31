23e20b08c5f9f103e3eee3fe65a7bf8bff646c3b
giirrr <ikl1121@naver.com>
2023-08-31 15:44:29 +0900

    nav바 꾸미기 전

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+// import { Button, Nav, Navbar, Container } from "react-bootstrap";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Button variant="primary">Primary</Button>{" "}
+    </div>
+  );
 }
 
 export default App;
