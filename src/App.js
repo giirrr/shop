ae3b7d3c45c02f91bc799a3e6404529b1a528c70
giirrr <ikl1121@naver.com>
2023-08-31 19:36:22 +0900

    컴포넌트 만들기,그림까지 바꿈

--- src/App.js
+++ src/App.js
@@ -29,50 +29,26 @@ function App() {
       <div className="main-bg"></div>
       <div className="container">
         <div className="row">
-          {shoes.map(function (a, i) {
-            return (
-              <div
-                className="col-md-4"
-                //함수를 어디에 만들지를 모르겠음 클릭 함수도 아니고.
-                onClick={() => {
-                  setId(i);
-                }}
-              >
-                <img
-                  // 그리고 사진은 어떻게 다르게 표시할지???
-                  src="https://codingapple1.github.io/shop/shoes3.jpg"
-                  width="80%"
-                />
-                <h4>{shoes[i].title}</h4>
-                <p>{shoes[i].price} 원</p>
-              </div>
-            );
-          })}
-          {/* 컴포넌트를 map으로 어케 하는지.. */}
-          <Item shoes={shoes} id={id} />
-          <Item shoes={shoes} id={1} />
-          <Item shoes={shoes} id={2} />
+          <Item shoes={shoes[0]} i={1} />
+          <Item shoes={shoes[1]} i={2} />
+          <Item shoes={shoes[2]} i={3} />
         </div>
       </div>
     </div>
   );
 }
+
 function Item(props) {
-  {
-    console.log(props.id);
-    {
-      return (
-        <div className="col-md-4" key={30}>
-          <img
-            src="https://codingapple1.github.io/shop/shoes3.jpg"
-            width="80%"
-          />
-          <h4>{props.shoes[props.id].title}</h4>
-          <p>{props.shoes[props.id].price} 원</p>
-        </div>
-      );
-    }
-  }
+  return (
+    <div className="col-md-4" key={30}>
+      <img
+        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price} 원</p>
+    </div>
+  );
 }
 
 export default App;
