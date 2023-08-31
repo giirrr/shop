d6ed2f905dd65f4e89d456e5b70294c3a3c3c1bc
giirrr <ikl1121@naver.com>
2023-08-31 21:34:43 +0900

    라우트 개판 된거 다시 정리 메인페이지는 APP.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,62 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { useState } from "react";
 import data from "./data.js";
-import Detail from "./detail.js";
-import Main from "./main.js";
+import Detail from "./pages/Detail.js";
 import { Routes, Route, Link } from "react-router-dom";
 // import { Button, Nav, Navbar, Container } from "react-bootstrap";
 
 function App() {
   let [shoes, setShoes] = useState(data);
-
   return (
     <div className="App">
+      <Navbar bg="primary" data-bs-theme="dark" className="shop_navbar">
+        <Container>
+          <Navbar.Brand href="#home" className="shop_navbarNavbar">
+            Shopping Pro
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Link to="/">홈</Link>
+            <Link to="/detail">상세페이지</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map(function (a, i) {
+                    return <Item shoes={shoes[i]} i={i} />;
+                  })}
+                </div>
+              </div>
+            </>
+          }
+        />
         <Route path="/detail" element={<Detail />} />
       </Routes>
+    </div>
+  );
+}
+
+function Item(props) {
+  return (
+    <div className="col-md-4" key={30}>
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price} 원</p>
     </div>
   );
 }
