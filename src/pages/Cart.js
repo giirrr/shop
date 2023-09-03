b254f93384741d8a9ca4d76407f9656d76c3495e
giirrr <ikl1121@naver.com>
2023-09-04 00:46:50 +0900

    useMemo

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,13 +2,36 @@ import { Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { changeName, increase } from "./../store/userSlice.js";
 import { addCount } from "./../store.js";
+import { memo, useState } from "react";
+
+// function Child() {
+//   console.log("재랜더링 됨");
+//   return <div>자식임</div>;
+// }
+//꼭 필요한 때만 재렌더링하려면 memo  -> 함수 밑에처럼 재선언
+let Child = memo(function () {
+  console.log("재렌더링됨");
+  return <div>자식임</div>;
+});
+//위에 처럼 함수 선언하고 전체를 memo로 감싸주면 됨
+//child라는 컴포넌트 여기로 전송되는 props가 변할 때만 재렌더링 해줌
+//기존 Porps랑 ==신규 props랑 계속 같은지 비교해볼듯
 
 function Cart() {
   let state = useSelector((state) => state);
   let dispatch = useDispatch();
   //store.js로 요청을 보내주는 함수 ,
+  let [count, setCount] = useState(0);
   return (
     <div>
+      <Child count={count}></Child>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
       <h6>
         {state.user.name} {state.user.age} 의 장바구니
       </h6>
